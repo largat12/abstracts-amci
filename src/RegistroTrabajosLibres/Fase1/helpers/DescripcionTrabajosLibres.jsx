de5e40ced7dd469392fa56e5ee42b8e1e41927c5
largat12 <gustavosanchez10b@gmail.com>
2023-02-06 22:25:58 -0500

    Correcion de descripcion general

--- src/RegistroTrabajosLibres/Fase1/helpers/DescripcionTrabajosLibres.jsx
+++ src/RegistroTrabajosLibres/Fase1/helpers/DescripcionTrabajosLibres.jsx
@@ -27,6 +27,19 @@ export const DescripcionTrabajosLibres = ({setactiveForm}) => {
             <li>No se aceptarán casos clínicos.</li>
             <li>Por lo menos uno de los autores del resumen sometido debe ser miembro de la AMCI para poder presentar en el congreso.</li>
             <li>En caso de que el autor presentador del resumen inscrito en el sistema cambie, debe notificarlo con tiempo a la organización, de lo contario no se tendrá en cuenta el trabajo para ser presentado en el congreso.</li>
+            <li>Cada resumen podrá participar en un eje temático, usted podrá seleccionar el que considere más afín a la temática de la investigación entre los siguientes
+              <ol>
+                <li><strong>Adultos – Respiratorio:</strong> diagnóstico, monitoreo, soporte y terapéutica, oxigenoterapia, ventilación mecánica.</li>
+                <li><strong>Adultos – Cardiovascular:</strong> diagnóstico, monitoreo, soporte, terapéutica, perfusión, asistencia circulatoria mecánica.</li>
+                <li><strong>Adultos - Neurocrítico:</strong> diagnóstico, monitoreo, soporte, terapéutica, pronóstico.</li>
+                <li><strong>Adultos - Sepsis:</strong> (diagnóstico, terapéutica, avances).</li>
+                <li><strong>Adultos - General:</strong> condiciones de alto riesgo obstétrico, metabólicas y soporte nutricional, autoinmunidad, microangiopatías entre otras.</li>
+                <li><strong>Adultos - innovación, organización, humanización y educación médica.</strong></li>
+                <li><strong>Cuidado Crítico Pediátrico</strong></li>
+                <li><strong>Enfermería</strong></li>
+                <li><strong>Terapia</strong></li>
+              </ol>
+            </li>
         </ul>
         <p>Para solucionar cualquier duda o si requiere información adicional comuníquese con el Comité Organizador del congreso a través del correo electrónico <a href="mailto:abstract@amci.org.co" target="_blank" rel="noreferrer">abstract@amci.org.co</a></p>
         <Container fluid className=' p-0'>
